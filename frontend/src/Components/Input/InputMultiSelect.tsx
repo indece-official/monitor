8ec46291117077f7371a7bb7c1625772234d1976
Stephan Lukas <stephan.lukas@indece.com>
2023-06-24 02:38:38 +0200

    feat: Refactoring InputMultiSelect (WIP)

--- frontend/src/Components/Input/InputMultiSelect.tsx
+++ frontend/src/Components/Input/InputMultiSelect.tsx
@@ -3,6 +3,8 @@ import { Field, FieldProps } from 'formik';
 import { InputSelectOption } from './InputSelect';
 
 import './Input.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 
 export interface InputMultiSelectProps
@@ -73,11 +75,11 @@ export class InputMultiSelect extends React.Component<InputMultiSelectProps, Inp
                 name={this.props.name}
                 validate={this._validate}>
                 {( fieldProps: FieldProps<Array<string>> ) => (
-                    <label className={`InputSelect ${fieldProps.field.value ? '' : 'empty'} ${this.props.disabled ? '' : 'disabled'} ${this.state.focussed ? 'focussed' : ''} ${fieldProps.meta.error ? 'error' : ''}`}>
-                        <div className='InputText-field'>
-                            <div className='InputSelect-label'>{this.props.label} {this.props.required ? '*' : ''}</div>
+                    <label className={`InputMultiSelect ${fieldProps.field.value ? '' : 'empty'} ${this.props.disabled ? '' : 'disabled'} ${this.state.focussed ? 'focussed' : ''} ${fieldProps.meta.error ? 'error' : ''}`}>
+                        <div className='InputMultiSelect-field'>
+                            <div className='InputMultiSelect-label'>{this.props.label} {this.props.required ? '*' : ''}</div>
                             
-                            <select
+                            {/*<select
                                 {...fieldProps.field}
                                 placeholder=''
                                 multiple={true}
@@ -88,7 +90,45 @@ export class InputMultiSelect extends React.Component<InputMultiSelectProps, Inp
                                 {this.props.options.map( ( option ) => (
                                     <option value={option.value} key={option.value}>{option.label}</option>
                                 ))}
-                            </select>
+                            </select>*/}
+                            <div className='InputMultiSelect-input'>
+                                {(fieldProps.field.value || []).map( ( value, i ) => (
+                                    <div className='InputMultiSelect-input-value' key={i}>
+                                        <div className='InputMultiSelect-input-value-label'>
+                                            {this.props.options.find( o => o.value === value )?.label || value }
+                                        </div>
+
+                                        <div
+                                            className='InputMultiSelect-input-value-action'
+                                            onClick={ ( ) => fieldProps.form.setFieldValue(fieldProps.field.name, (fieldProps.field.value || []).filter( s => s !== value))}>
+                                            <FontAwesomeIcon icon={faTimes} />
+                                        </div>
+                                    </div>
+                                ))}
+
+                                <div className='InputMultiSelect-input-select'>
+                                    <input
+                                        type='text'
+                                    />
+
+                                    <div className='InputMultiSelect-dropdown'>
+                                        {this.props.options.filter( o => !(fieldProps.field.value || []).includes(o.value) ).map( ( option ) => (
+                                            <div
+                                                className='InputMultiSelect-dropdown-option'
+                                                key={option.value}
+                                                onClick={ ( ) => fieldProps.form.setFieldValue(fieldProps.field.name, [...(fieldProps.field.value || []), option.value])}>
+                                                {option.label}
+                                            </div>
+                                        ))}
+
+                                        {this.props.options.filter( o => !(fieldProps.field.value || []).includes(o.value) ).length === 0 ?
+                                            <div className='InputMultiSelect-dropdown-empty'>
+                                                No options found
+                                            </div>
+                                        : null}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
                         {fieldProps.meta.error ?
